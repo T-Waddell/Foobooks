09a23439d31fe708e96019a20180852b6f6a28f3
T-Waddell <tara.waddell2@gmail.com>
2018-10-29 22:31:16 -0500

    Added routes and controllers.

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,45 @@
 |
 */
 
+//original
+/*
 Route::get('/', function () {
-    return view('abc');
-    #return view('welcome');
+    #return view('abc');
+    return view('welcome');
 });
+*/
+Route::get('/', 'WelcomeController');
+
+//with multiple parameters, they correspond with the variables passed to the closure function in the order they appear in the URI.
+/*Route::get('/books/{category}/{title}', function ($category, $title) {
+    return 'You are viewing the book: '.$title.' in the category '.$category;
+}); */
+
+//? makes the URL optional. method 1
+/*Route::get('/books/{title?}', function ($title = null) {
+    if($title == null) {
+        return "You did not specify a title.";
+    }
+    else {
+        return 'You are viewing the book: '.$title;
+    }
+});
+*/
+
+//By coding a specific action for the /books route with no title, we no longer need
+//to make the title optional.
+//original method, replaced by the controller below:
+/*Route::get('/books/{title}', function ($title) {
+        return 'You are viewing the book: '.$title;
+});
+*/
+Route::get('/books/{title}', 'BookController@show');
+
+//original method. replaced by the controller below:
+/*Route::get('/books', function () {
+    return 'Here are all the books...';
+});
+*/
+
+//second thing we pass in is a string of the controller and the method within it that we want to invoke.
+Route::get('/books', 'BookController@index');
